f8d112dd42b907883f49036784e44cde4299115e
AttabiqKhan <attabiqkhan@gmail.com>
2024-10-07 11:08:17 +0500

    Archiving and building for distribution

---
+++ build/iOSSimulator.xcarchive/Products/Library/Frameworks/SimpleMultiplication.framework/Headers/SimpleMultiplication.h
@@ -0,0 +1,18 @@
+//
+//  SimpleMultiplication.h
+//  SimpleMultiplication
+//
+//  Created by Attabiq Khan on 07/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMultiplication.
+FOUNDATION_EXPORT double SimpleMultiplicationVersionNumber;
+
+//! Project version string for SimpleMultiplication.
+FOUNDATION_EXPORT const unsigned char SimpleMultiplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMultiplication/PublicHeader.h>
+
+
